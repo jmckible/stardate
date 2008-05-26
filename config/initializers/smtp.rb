3bc247b4512ae510633d5504b3f87c2d4df0e4c8
Jordan McKible <jordan@mckible.com>
2008-05-25 21:08:26 -0700

    Proper error if mail.yml doesn't exist.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,20 +1,20 @@
 begin
-require 'smtp_tls'
+  require 'smtp_tls'
 
-mail_yml = YAML.load_file(RAILS_ROOT+'/config/mail.yml')
-mail_env = mail_yml[ENV['RAILS_ENV']]
+  mail_yml = YAML.load_file(RAILS_ROOT+'/config/mail.yml')
+  mail_env = mail_yml[ENV['RAILS_ENV']]
 
-ActionMailer::Base.smtp_settings = {
-  :authentication => mail_env['authentication'],
-  :address        => mail_env['address'],
-  :port           => mail_env['port'],
-  :user_name      => mail_env['user_name'],
-  :password       => mail_env['password']
-}
+  ActionMailer::Base.smtp_settings = {
+    :authentication => mail_env['authentication'],
+    :address        => mail_env['address'],
+    :port           => mail_env['port'],
+    :user_name      => mail_env['user_name'],
+    :password       => mail_env['password']
+  }
 
-ExceptionNotifier.exception_recipients = mail_env[:exception_recipient]
-ExceptionNotifier.sender_address       = mail_env[:exception_sender]
+  ExceptionNotifier.exception_recipients = mail_env[:exception_recipient]
+  ExceptionNotifier.sender_address       = mail_env[:exception_sender]
 
-rescue
-  logger.warn "Mail sending not configured.  Check config/mail.yml"
+rescue Errno::ENOENT
+  $stderr.puts '[WARN] Missing config/mail.yml. Mail will not be sent.'
 end

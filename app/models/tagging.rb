6cdb09b20e716c8c383e93b2afb4eac25514856d
Jordan McKible <jordan@mckible.com>
2008-06-06 15:48:47 -0700

    Convert Tagging test to specs.

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,7 +1,7 @@
 class Tagging < ActiveRecord::Base
-  belongs_to :item
   belongs_to :tag
+  belongs_to :item
   
-  validates_presence_of :tag, :item
+  validates_presence_of   :tag_id, :item_id
   validates_uniqueness_of :tag_id, :scope=>:item_id
 end
---
+++ spec/models/tagging_spec.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+#####################################################################
+#                     R E L A T I O N S H I P S                     #
+#####################################################################
+describe Tagging, 'relationships' do
+  it 'should belong to a tag' do
+    taggings(:pizza_food).tag.should == tags(:food)
+  end
+  
+  it 'should belong to and item' do
+    taggings(:pizza_food).item.should == items(:pizza)
+  end
+end
+
+#####################################################################
+#                       V A L I D A T I O N S                       #
+#####################################################################
+describe Tagging, 'validations' do
+  it' should belong to a tag' do
+    Tagging.new.should have(1).error_on(:tag_id)
+  end
+  
+  it 'should belong to an item' do
+    Tagging.new(:item_id=>nil).should have(1).error_on(:item_id)
+  end
+  
+  it 'should tag an item uniquely' do
+    tagging = taggings(:pizza_food).clone
+    tagging.should have(1).error_on(:tag_id)
+  end
+end

551a43420aa95f08367832647e9aba2619f11c41
Jordan McKible <jordan@mckible.com>
2008-06-06 15:40:20 -0700

    Convert Tag test to spec.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,12 +1,12 @@
 class Tag < ActiveRecord::Base
   has_many :taggings, :dependent=>:destroy
-  has_many :items, :through=>:taggings 
+  has_many :items,    :through=>:taggings 
   
   before_validation :strip_spaces
   def strip_spaces
     self.name = name.strip unless name.nil?
   end
   
-  validates_presence_of :name
+  validates_presence_of   :name
   validates_uniqueness_of :name
 end
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+#####################################################################
+#                     R E L A T I O N S H I P S                     #
+#####################################################################
+describe Tag, 'relationships' do
+  it 'should have many taggings' do
+    tags(:food).should have(3).taggings
+  end
+  
+  it 'should have many items' do
+    tags(:food).should have(3).items
+  end
+end
+
+#####################################################################
+#                    O B J E C T    M E T H O D S                   #
+#####################################################################
+describe Tag do
+  it 'should strip spaces before save' do
+    tag = Tag.new :name=>' spaces '
+    tag.save
+    tag.name.should == 'spaces'
+  end
+end
+
+#####################################################################
+#                       V A L I D A T I O N S                       #
+#####################################################################
+describe Tag, 'validations' do
+  it 'should have a name' do
+    Tag.new.should have(1).error_on(:name)
+  end
+  
+  it 'should have a unique name' do
+    tag = tags(:food).clone
+    tag.should have(1).error_on(:name)
+  end
+end

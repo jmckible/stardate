9559031c77162fbb590b522fc5f52ac6489a9565
Jordan McKible <jordan@mckible.com>
2008-10-05 11:24:51 -0700

    Reduce activity graphs views to a single builder file.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -9,18 +9,6 @@ class GraphsController < ApplicationController
   # GET /graphs/activity.xml/2007/1/1/31
   def activity
     period_assign_to_today
-    respond_to do |format|
-      format.xml do
-        case @period.last - @period.first
-        when 0..31
-          render :action=>:activity_daily
-        when 32..140
-          render :action=>:activity_weekly
-        else
-          render :action=>:activity_monthly
-        end
-      end
-    end
   end
   
 end

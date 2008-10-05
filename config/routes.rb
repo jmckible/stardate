8ef684b1f8bf0c9680d4d87f820a56f3b6993518
Jordan McKible <jordan@mckible.com>
2008-10-05 11:32:47 -0700

    All job/paycheck/task controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,33 @@
 ActionController::Routing::Routes.draw do |map|
-  ##########################
-  #      G R A P H S       #
-  ##########################
-  map.activity_graph 'graphs/activity.:format/:year/:month/:day/:period',
-    :controller=>'graphs',
-    :action=>'activity',
-    :requirement=>{:format=>/xml/, :year=>/(19|20)\d\d/, :month=>/[01]?\d/, :day=>/[0-3]?\d/, :period=>/\d+/},
-    :year=>nil,
-    :month=>nil,
-    :day=>nil,
-    :period=>nil
-    
-  ##########################
-  #   R E S O U R C E S    #
-  ##########################  
   map.resources :items
-  map.resources :jobs, :has_many=>:paychecks
   map.resources :recurrings
-  map.resources :tasks
   map.resources :users
-    
-  ##########################
-  #       L O G I N        #
-  ##########################
-  map.login 'login', :controller=>'sessions', :action=>'login'
-  map.logout 'logout', :controller=>'sessions', :action=>'logout'
-  map.authenticate 'authenticate', :controller=>'sessions', :action=>'authenticate', :conditions=>{:method=>:post}
   
-  ##########################
-  #        H O M E         #
-  ##########################
-  map.home 'home', :controller=>'home'
+  map.activity_graph 'graphs/activity.:format/:year/:month/:day/:period',
+    :controller  => 'graphs',
+    :action      => 'activity',
+    :requirement => {:format=>/xml/, :year=>/(19|20)\d\d/, :month=>/[01]?\d/, :day=>/[0-3]?\d/, :period=>/\d+/},
+    :year        => nil,
+    :month       => nil,
+    :day         => nil,
+    :period      => nil
+    
   map.register 'register/:year/:month/:day/:period',
-    :controller=>'register',
-    :action=>'index',
-    :requirements=>{:year=>/(19|20)\d\d/, :month=>/[01]?\d/, :day=>/[0-3]?\d/, :period=>/\d+/},
-    :year=>nil,
-    :month=>nil,
-    :day=>nil,
-    :period=>nil
+    :controller   => 'register',
+    :action       => 'index',
+    :requirements => {:year=>/(19|20)\d\d/, :month=>/[01]?\d/, :day=>/[0-3]?\d/, :period=>/\d+/},
+    :year         => nil,
+    :month        => nil,
+    :day          => nil,
+    :period       => nil
+
+  map.with_options :controller=>'sessions' do |s|
+    map.login        'login',        :action=>'login'
+    map.logout       'logout',       :action=>'logout'
+    map.authenticate 'authenticate', :action=>'authenticate', :conditions=>{:method=>:post}
+  end
+  
+  map.home 'home', :controller=>'home'
   map.welcome '', :controller=>'public'
+  
 end

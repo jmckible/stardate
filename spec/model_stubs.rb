a7750d9c071d73c211bd80a0239e004f135d20ec
Jordan McKible <jordan@mckible.com>
2008-10-23 20:40:18 -0700

    Convert Item specs.

--- spec/model_stubs.rb
+++ spec/model_stubs.rb
@@ -1,46 +1,46 @@
-module ModelStubbing
+ModelStubbing.define_models do 
   
-  define_models do
-    time 2008, 1, 1
-    
-    model Item do
-      stub :date=>current_time.to_date, :description=>'What I bought', :value=>-10, 
-           :user=>all_stubs(:user), :vendor=>all_stubs(:vendor)
-    end
-    
-    model Job do
-      stub :name=>'Default', :user=>all_stubs(:user), :created_at=>current_time,
-           :active=>true, :rate=>9.5, :vendor=>all_stubs(:vendor)
-    end
-    
-    model Paycheck do
-      stub :job=>all_stubs(:job), :description=>'Got paid', :value=>300, :item=>all_stubs(:item)
-    end
-    
-    model Recurring do
-      stub :day=>1, :value=>-100, :user=>all_stubs(:user), :description=>'Recurring'
-    end
-    
-    model Tagging do
-      stub :taggable=>all_stubs(:item), :tag=>all_stubs(:tag)
-    end
-    
-    model Tag do
-      stub :name=>'default'
-    end
-    
-    model Task do
-      stub :job=>all_stubs(:job), :date=>current_time.to_date, :minutes=>60, :paycheck=>all_stubs(:paycheck)
-    end
-    
-    model User do
-      stub :email=>'default@email.com', :time_zone=>'Pacific Time (US & Canada)',
-           :password_salt=>'salt', :password_hash=>'f438229716cab43569496f3a3630b3727524b81b'
-    end
-    
-    model Vendor do
-      stub :name=>'Default'
-    end
+  time 2008, 1, 1
+  
+  model User do
+    stub :email=>'default@email.com', :time_zone=>'Pacific Time (US & Canada)',
+         :password_salt=>'salt', :password_hash=>'f438229716cab43569496f3a3630b3727524b81b'
+    stub :other, :email=>'other@email.com'
+  end
+  
+  model Vendor do
+    stub :name=>'Default'
+  end
+  
+  model Tag do
+    stub :name=>'default'
+  end
+  
+  model Item do
+    stub :date=>current_time.to_date, :description=>'What I bought', :value=>-10, 
+         :user=>users(:default), :vendor=>vendors(:default)
+  end
+  
+  model Job do
+    stub :name=>'Default', :user=>users(:default), :created_at=>current_time,
+         :active=>true, :rate=>9.5, :vendor=>vendors(:default)
+  end
+  
+  model Paycheck do
+    stub :job=>jobs(:default), :description=>'Got paid', :value=>300, :item=>items(:default)
+  end
+  
+  model Recurring do
+    stub :day=>1, :value=>-100, :user=>users(:default), :description=>'Recurring'
+  end
+  
+  model Tagging do
+    stub :taggable=>items(:default), :tag=>tags(:default)
+  end
+  
+  model Task do
+    stub :job=>jobs(:default), :date=>current_time.to_date, :minutes=>60, 
+         :paycheck=>paychecks(:default)
   end
   
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,17 +3,19 @@ require File.dirname(__FILE__) + '/../spec_helper'
 #####################################################################
 #                     R E L A T I O N S H I P S                     #
 #####################################################################
-describe Item, 'relationships' do 
+describe Item, 'relationships' do
+  define_models
+  
   it 'should belong to a user' do
-    items(:pizza).user.should == users(:jordan)
+    items(:default).user.should == users(:default)
   end
-  
+
   it 'should belong to a paycheck' do
-    items(:starbucks_last_week).paycheck.should == paychecks(:starbucks_last_week)
+    items(:default).paycheck.should == paychecks(:default)
   end
-  
+
   it 'should belong to a vendor' do
-    items(:pizza).vendor.should == vendors(:sals)
+    items(:default).vendor.should == vendors(:default)
   end
 end
 
@@ -21,39 +23,40 @@ end
 #                             T A G G I N G                         #
 #####################################################################
 describe Item, 'tagging' do
+  define_models
   
-  before(:each) do
-    @item = items(:pizza)
+  before do
+    @item = items(:default)
   end
-  
+
   it 'should have many taggings' do
-    @item.should have(2).taggings
+    @item.should have(1).taggings
   end
-  
+
   it 'should have many tags alphabetically' do
-    @item.tags.should == tags(:food, :pizza)
+    @item.tags.should == [tags(:default)]
   end
-  
+
   it 'should have a tag list' do
-    @item.tag_list.should == 'food, pizza'
+    @item.tag_list.should == 'default'
   end
-  
+
   it 'should add a new tag via tag list' do
     running {
       running {
-        @item.tag_list = 'pizza, food, cupcake'
+        @item.tag_list = 'default, breakfast'
         @item.save
-        @item.reload.tag_list.should == 'cupcake, food, pizza'
+        @item.reload.tag_list.should == 'breakfast, default'
       }.should change(Tag, :count).by(1) 
     }.should change(Tagging, :count).by(1)
   end
-  
+
   it 'should empty tag list when nothing is passed' do
     running {
       @item.tag_list = ''
       @item.save
       @item.should have(0).tags
-    }.should change(Tagging, :count).by(-2)
+    }.should change(Tagging, :count).by(-1)
   end
 end
 
@@ -61,8 +64,9 @@ end
 #                            S C O P E                              #
 #####################################################################
 describe Item, 'scope' do
+  define_models
   it 'should have a during scope' do
-    Item.should have(3).on(Date.new(2008, 1, 15))
+    Item.should have(1).on(current_time.to_date)
   end
 end
 
@@ -70,19 +74,20 @@ end
 #                         P R O T E C T I O N                       #
 #####################################################################
 describe Item, 'protections' do
+  define_models
   
-  before(:each) do
-    @item = items(:pizza)
+  before do
+    @item = items(:default)
   end
-  
+
   it 'should not update user_id through mass assignment' do
-    @item.update_attributes :user_id=>users(:scott).id
-    @item.user_id.should_not == users(:scott).id
+    @item.update_attributes :user_id=>users(:other).id
+    @item.user_id.should_not == users(:other).id
   end
-  
+
   it 'should not update user through mass assignment' do
-    @item.update_attributes :user=>users(:scott)
-    @item.user.should_not == users(:scott)
+    @item.update_attributes :user=>users(:other)
+    @item.user.should_not == users(:other)
   end
 end
 
@@ -90,11 +95,12 @@ end
 #                       V A L I D A T I O N S                       #
 #####################################################################
 describe Item, 'validations' do
+  define_models
   
   it 'should have a date' do
     Item.new.should have(1).error_on(:date)
   end
-  
+
   it 'should have a user_id' do
     Item.new.should have(1).error_on(:user_id)
   end
@@ -104,9 +110,10 @@ end
 #                       D E S T R U C T I O N                       #
 #####################################################################
 describe Item, 'destruction' do
+  define_models
   
   it 'should nullify paycheck on destroy' do
-    items(:starbucks_last_week).destroy
-    paychecks(:starbucks_last_week).item.should be_nil
+    items(:default).destroy
+    paychecks(:default).item.should be_nil
   end
 end

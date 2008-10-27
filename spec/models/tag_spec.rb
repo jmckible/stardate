0319cfd845f2f537b995c897e86584e2f6e7a576
Jordan McKible <jordan@mckible.com>
2008-10-27 10:21:24 -0700

    Port Tag spec.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,35 +1,35 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-#####################################################################
-#                     R E L A T I O N S H I P S                     #
-#####################################################################
-describe Tag, 'relationships' do
-  it 'should have many taggings' do
-    tags(:food).should have(3).taggings
-  end
-end
-
-#####################################################################
-#                    O B J E C T    M E T H O D S                   #
-#####################################################################
 describe Tag do
+  define_models
+  before { @tag = tags(:default) }
+  
+  #####################################################################
+  #                     R E L A T I O N S H I P S                     #
+  #####################################################################
+  it 'should have many taggings' do
+    @tag.should have(1).taggings
+  end
+
+  #####################################################################
+  #                    O B J E C T    M E T H O D S                   #
+  #####################################################################
   it 'should strip spaces before save' do
     tag = Tag.new :name=>' spaces '
     tag.save
     tag.name.should == 'spaces'
   end
-end
 
-#####################################################################
-#                       V A L I D A T I O N S                       #
-#####################################################################
-describe Tag, 'validations' do
+  #####################################################################
+  #                       V A L I D A T I O N S                       #
+  #####################################################################
   it 'should have a name' do
     Tag.new.should have(1).error_on(:name)
   end
-  
+
   it 'should have a unique name' do
-    tag = tags(:food).clone
+    tag = @tag.clone
     tag.should have(1).error_on(:name)
   end
 end
+

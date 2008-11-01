0ee4969affa6d4aa2e2d28bf7880c0e8d0b5bef7
Jordan McKible <jordan@mckible.com>
2008-11-01 15:04:37 -0700

    Port ItemsController spec.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,17 +1,23 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+describe ItemsController, 'without logging in' do
+  it 'handles /' do
+    get :index
+    response.should redirect_to(new_session_path)
+  end
+end
+
 describe ItemsController do
+  define_models
+  before do
+    login_as :default
+    @item = items(:default)
+  end
   
   #############################################################################
   #                                  I N D E X                                #
   #############################################################################
-  it 'handles / without logging in' do
-    get :index
-    response.should redirect_to(new_session_path)
-  end
-  
-  it 'handles / with logged in user' do
-    login_as :jordan
+  it 'handles /' do
     get :index
     response.should be_success
   end
@@ -20,8 +26,7 @@ describe ItemsController do
   #                                    S H O W                                #
   #############################################################################
   it 'handles /items/:id with GET' do
-    login_as :jordan
-    get :show, :id=>items(:pizza)
+    get :show, :id=>@item
     response.should be_success
   end
   
@@ -29,7 +34,6 @@ describe ItemsController do
   #                                   C R E A T E                             #
   #############################################################################
   it 'handles /items with valid params and POST' do
-    login_as :jordan
     running {
       running {
         post :create, :item=>{:explicit_value=>20, :date=>Date.today, :description=>'twenty' }, :vendor=>{:name=>'new'}
@@ -41,7 +45,6 @@ describe ItemsController do
   end
   
   it 'handles /items with no params and POST' do
-    login_as :jordan
     running {
       post :create, :item=>{}
       response.should redirect_to(items_path)
@@ -49,7 +52,6 @@ describe ItemsController do
   end
   
   it 'handles /items with no vendor and POST' do
-    login_as :jordan
     running {
       post :create, :item=>{:explicit_value=>20, :date=>Date.today, :description=>'twenty' }
       assigns(:item).value.should == -20
@@ -61,20 +63,16 @@ describe ItemsController do
   #                                   U P D A T E                             #
   #############################################################################
   it 'handles /items/:id with valid params and PUT' do
-    login_as :jordan
-    item = items(:pizza)
-    put :update, :id=>item, :item=>{:description=>'new'}, :vendor=>{:name=>vendors(:panera).name}
-    item.reload.description.should == 'new'
-    item.vendor.should == vendors(:panera)
+    put :update, :id=>@item, :item=>{:description=>'new'}, :vendor=>{:name=>vendors(:other).name}
+    @item.reload.description.should == 'new'
+    @item.vendor.should == vendors(:other)
     response.should redirect_to(items_path)
   end
   
   it 'handles /items/:id with emptying of vendor' do
-    login_as :jordan
-    item = items(:pizza)
-    put :update, :id=>item, :item=>{:description=>'new'}
-    item.reload.description.should == 'new'
-    item.vendor.should be_nil
+    put :update, :id=>@item, :item=>{:description=>'new'}
+    @item.reload.description.should == 'new'
+    @item.vendor.should be_nil
     response.should redirect_to(items_path)
   end
   
@@ -82,9 +80,8 @@ describe ItemsController do
   #                                 D E L E T E                               #
   #############################################################################
   it 'handles /items/:id with DELETE' do
-    login_as :jordan
     running {
-      delete :destroy, :id=>items(:pizza)
+      delete :destroy, :id=>@item
       response.should redirect_to(items_path)
     }.should change(Item, :count).by(-1)
   end

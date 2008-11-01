4dd354324e83b1f308aa25c680f78f3448b71761
Jordan McKible <jordan@mckible.com>
2008-11-01 14:52:21 -0700

    Port Vendor spec.

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,12 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Vendor, 'validations' do
+  define_models
+  
   it 'should have a name' do
     Vendor.new.should have(1).error_on(:name)
   end
   
   it 'should have a unique name' do
-    vendor = vendors(:starbucks).clone
+    vendor = vendors(:default).clone
     vendor.should have(1).error_on(:name)
   end
 end

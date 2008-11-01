3157ab9671298503821ca3835e2bd124e7d0f938
Jordan McKible <jordan@mckible.com>
2008-11-01 15:08:25 -0700

    Port RegisterController.

--- spec/controllers/register_controller_spec.rb
+++ spec/controllers/register_controller_spec.rb
@@ -1,9 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe RegisterController do
+  define_models
+  before { login_as :default }
   
   it 'handles /register with GET' do
-    login_as :jordan
     get :index
     assigns(:period).first.should == Date.new(Date.today.year, Date.today.month, 1)
     assigns(:period).last.should == Date.civil(Date.today.year, Date.today.month, -1)
@@ -11,7 +12,6 @@ describe RegisterController do
   end
   
   it 'handles /register/2007/1 with GET' do
-    login_as :jordan
     get :index, :year=>2007, :month=>1
     assigns(:period).first.should == Date.new(2007, 1, 1)
     assigns(:period).last.should == Date.civil(2007, 1, 31)
@@ -19,7 +19,6 @@ describe RegisterController do
   end
   
   it 'handles /register with POST' do
-    login_as :jordan
     post :index, :date=>{:year=>2008, :month=>10}
     assigns(:period).first.should == Date.new(2008, 10, 1)
     assigns(:period).last.should == Date.civil(2008, 10, 31)

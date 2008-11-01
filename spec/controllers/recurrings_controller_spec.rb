d0cd7cc7c2d405fc72c02a2acfe9dfcde8925337
Jordan McKible <jordan@mckible.com>
2008-11-01 15:07:36 -0700

    Port RecurringsController spec.

--- spec/controllers/recurrings_controller_spec.rb
+++ spec/controllers/recurrings_controller_spec.rb
@@ -1,15 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe RecurringsController do
+  define_models
+  before { login_as :default }
   
   it 'handles /recurrings with GET' do
-    login_as :jordan
     get :index
     response.should be_success
   end
   
   it 'handles /recurrings/new with GET' do
-    login_as :jordan
     get :new
     response.should be_success
   end

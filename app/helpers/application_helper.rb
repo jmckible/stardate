0997da0f1d1a7e39ca2d4d44047b421834186bd2
Jordan McKible <jordan@mckible.com>
2008-11-04 17:41:29 -0800

    Clean out all UI elements.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def color_value(value)
-    return '&nbsp;' if value.nil?
-    if value < 0
-      content_tag :span, number_with_delimiter(value*-1), :class=>'red'
-    elsif value > 0
-      content_tag :span, number_with_delimiter(value), :class=>'green'
-    else
-       '&nbsp;'
-    end
-  end
-  
-  def color_money(value)
-    return '0' if value.nil?
-    if value < 0
-      content_tag :span, number_with_delimiter(value*-1), :class=>'red'
-    elsif value > 0
-      content_tag :span, number_with_delimiter(value), :class=>'green'
-    else
-       '0'
-    end
-  end
   
   def humanize_period(period)
     if period.first.day == 1 and period.last == Date.civil(period.first.year, period.first.month, -1)
@@ -39,8 +18,5 @@ module ApplicationHelper
     minutes = "0#{minutes}" if minutes < 10
     [hours, minutes].join(':')
   end
-  
-  def footer
-    "ActiveBudget is available under the #{link_to 'MIT license', 'http://www.opensource.org/licenses/mit-license.php'} and managed by #{link_to 'Jordan McKible', 'http://jordan.mckible.com'}"
-  end
+
 end

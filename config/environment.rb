7ae029208bc4bf96f6bf1905ac8204ea7a34e458
Jordan McKible <jordan@mckible.com>
2008-11-30 14:07:58 -0800

    Require HTTParty gem.

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
+  config.gem 'httparty'
   
   # Skip frameworks you're not going to use (only works if using vendor/rails)
   # config.frameworks -= [ :action_web_service, :action_mailer ]

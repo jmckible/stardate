a4167393fb3d8307623b62abc42b3174d04d49c1
Jordan McKible <jordan@mckible.com>
2008-11-30 15:32:19 -0800

    ezcrypto gem required for lucifer.

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
   # config.gem 'httparty'
+  # config.gem 'ezcrypto'
   
   # Skip frameworks you're not going to use (only works if using vendor/rails)
   # config.frameworks -= [ :action_web_service, :action_mailer ]

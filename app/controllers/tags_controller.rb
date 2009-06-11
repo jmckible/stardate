e56266c4fb4ee291367d8a08bd155c663b893e6c
Jordan McKible <jordan@mckible.com>
2009-06-10 23:34:52 -0700

    Show proper sums on paginated pages.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,6 +9,8 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_permalink params[:id]
     @items = current_user.items.find_tagged_with(@tag.name, :order=>'date desc').paginate :page=>params[:page]
+    @items_count = current_user.items.find_tagged_with(@tag.name).size
+    @items_sum = current_user.items.find_tagged_with(@tag.name).sum &:value
   end
   
 end

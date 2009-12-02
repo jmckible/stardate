5b0abafef85e89b9fe45d94805916433d7b78796
Jordan McKible <jordan@mckible.com>
2009-12-01 17:33:46 -0800

    Simplify requiring spec_helper.

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe Job do
   before { @job = jobs(:default) }
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe Vendor, 'relationships' do
   before { @vendor = vendors(:default) }

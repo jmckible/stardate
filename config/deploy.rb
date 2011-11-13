87cb16331ed2a6107ff59a7e6eb3e207d8ca4d13
Jordan McKible <jordan@mckible.com>
2011-11-12 23:24:11 -0800

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,39 +16,18 @@ set :branch,            'master'
 set :deploy_via,        :remote_cache
 set :git_shallow_clone, 1
 
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles=>:app, :except=>{ :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
 after 'deploy:update_code', 'symlink_configs'
 task :symlink_configs, :roles=>:app do
   run <<-CMD
     cd #{release_path} &&
-    ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml &&
-    ln -nfs #{shared_path}/config/key.yml #{release_path}/config/key.yml
+    ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
   CMD
-end
-
-after 'symlink_configs', 'migrate'
-task :migrate, :roles=>:db, :only=>{:primary=>true} do
-  rake           = fetch :rake,           'rake'
-  rails_env      = fetch :rails_env,      'production'
-  migrate_env    = fetch :migrate_env,    ''
-  migrate_target = fetch :migrate_target, :latest
-
-  directory = case migrate_target.to_sym
-    when :current then current_path
-    when :latest  then current_release
-    else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
-  end
-
-  run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} #{migrate_env} db:migrate"
-end
-
-namespace :deploy do
-  desc 'Restarting passenger with restart.txt'
-  task :restart, :roles=>:app, :except=>{:no_release=>true} do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
- 
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles=>:app do ; end
-  end
 end

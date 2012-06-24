0daa8b39865e2e4f203cf2142240ccfbe9b36665
Jordan McKible <jordan@mckible.com>
2012-06-24 12:15:34 -0700

    Again.

--- db/migrate/20120623232206_decompose_workouts.rb
+++ db/migrate/20120623232206_decompose_workouts.rb
@@ -1,15 +1,15 @@
 class DecomposeWorkouts < ActiveRecord::Migration
   def up
-    # add_column :workouts, :bike, :boolean, :default=>false
-    # add_index  :workouts, :bike
-    # add_column :workouts, :elliptical, :boolean, :default=>false
-    # add_index  :workouts, :elliptical
-    # add_column :workouts, :nike, :boolean, :default=>false
-    # add_index  :workouts, :nike
-    # add_column :workouts, :p90x, :boolean, :default=>false
-    # add_index  :workouts, :p90x
-    # add_column :workouts, :run, :boolean, :default=>false
-    # add_index  :workouts, :run
+    add_column :workouts, :bike, :boolean, :default=>false
+    add_index  :workouts, :bike
+    add_column :workouts, :elliptical, :boolean, :default=>false
+    add_index  :workouts, :elliptical
+    add_column :workouts, :nike, :boolean, :default=>false
+    add_index  :workouts, :nike
+    add_column :workouts, :p90x, :boolean, :default=>false
+    add_index  :workouts, :p90x
+    add_column :workouts, :run, :boolean, :default=>false
+    add_index  :workouts, :run
     
     Workout.update_all "bike = '1'", "type = 'Bike'"
     Workout.update_all "elliptical = '1'", "type = 'Elliptical'"

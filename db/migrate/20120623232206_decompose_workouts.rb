55744f2e01ae0dd0f03c4a5b15a380d184eee2a8
Jordan McKible <jordan@mckible.com>
2012-06-24 10:40:30 -0700

    pg migrate.

--- db/migrate/20120623232206_decompose_workouts.rb
+++ db/migrate/20120623232206_decompose_workouts.rb
@@ -1,21 +1,21 @@
 class DecomposeWorkouts < ActiveRecord::Migration
   def up
-    add_column :workouts, :bike, :boolean, :default=>false
-    add_index  :workouts, :bike
-    add_column :workouts, :elliptical, :boolean, :default=>false
-    add_index  :workouts, :elliptical
-    add_column :workouts, :nike, :boolean, :default=>false
-    add_index  :workouts, :nike
-    add_column :workouts, :p90x, :boolean, :default=>false
-    add_index  :workouts, :p90x
-    add_column :workouts, :run, :boolean, :default=>false
-    add_index  :workouts, :run
+    # add_column :workouts, :bike, :boolean, :default=>false
+    # add_index  :workouts, :bike
+    # add_column :workouts, :elliptical, :boolean, :default=>false
+    # add_index  :workouts, :elliptical
+    # add_column :workouts, :nike, :boolean, :default=>false
+    # add_index  :workouts, :nike
+    # add_column :workouts, :p90x, :boolean, :default=>false
+    # add_index  :workouts, :p90x
+    # add_column :workouts, :run, :boolean, :default=>false
+    # add_index  :workouts, :run
     
-    Workout.update_all "bike = 1", "type = 'Bike'"
-    Workout.update_all "elliptical = 1", "type = 'Elliptical'"
-    Workout.update_all "nike = 1", "type = 'Nike'"
-    Workout.update_all "p90x = 1", "type = 'P90x'"
-    Workout.update_all "run = 1",  "type = 'Run'"
+    Workout.update_all "bike = '1'", "type = 'Bike'"
+    Workout.update_all "elliptical = '1'", "type = 'Elliptical'"
+    Workout.update_all "nike = '1'", "type = 'Nike'"
+    Workout.update_all "p90x = '1'", "type = 'P90x'"
+    Workout.update_all "run = '1'",  "type = 'Run'"
     
     remove_column :workouts, :type
   end

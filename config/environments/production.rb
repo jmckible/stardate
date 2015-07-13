22fa1f2ca41f61aed96fe53891f28dc287e6c60f
Jordan McKible <jordan@mckible.com>
2015-07-13 11:42:43 -0700

    `serve_static_files` deprecation warning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Stardate::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -40,7 +40,7 @@ Stardate::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.compile = true
-    
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 

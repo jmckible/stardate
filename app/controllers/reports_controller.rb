dea6b3979af4c9ee79ef2c2d9bdd079ba8ae7332
Jordan McKible <jordan@mckible.com>
2016-03-20 14:22:11 -0700

    Seperate exceptional reports.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,10 +4,14 @@ class ReportsController < ApplicationController
   def index
     set_period
 
-    @income      = @household.sum_income @period
-    @exceptional = @household.transactions.income_credit.during(@period).where(exceptional: true).sum(:amount)
-    @expenses    = @household.sum_expenses(@period) * -1
-    @net         = @income + @expenses
+    @income = @household.sum_income @period
+    @exceptional_income = @household.sum_non_exceptional_income @period
+
+    @expenses = @household.sum_expenses(@period) * -1
+    @exceptional_expenses = @household.sum_non_exceptional_expenses(@period) * -1
+
+    @net = @income + @expenses
+    @exceptional_net = @exceptional_income + @exceptional_expenses
 
     @expense_accounts = @household.expense_accounts.where(transactions: { date: @period })
     @expense_tags     = @household.expense_tags.where(transactions: { date: @period })
